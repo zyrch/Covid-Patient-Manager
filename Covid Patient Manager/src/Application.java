c741d648abb0b1be8a3f94c6f827569577000bb5
Rupanshu Yadav <58172798+zyrch@users.noreply.github.com>
2020-09-06 03:57:13 +0530

    Add files via upload

---
+++ Covid Patient Manager/src/Application.java
@@ -0,0 +1,89 @@
+import java.util.*;
+
+public class Application {
+
+    static Institutes institutes;
+    static Camp camp;
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        institutes = new Institutes();
+        camp = new Camp();
+
+        int NumberOfPatients = in.nextInt();
+        int NumberOfAdmittedPatients = 0;
+        for (int i = 0; i < NumberOfPatients; ++i) {
+            String name = in.next();
+            float temperature = in.nextFloat();
+            int oxygen = in.nextInt();
+            int age = in.nextInt();
+            camp.addPatient(new Patient(name, temperature, oxygen, age));
+        }
+
+//       process queries
+
+        while(NumberOfAdmittedPatients < NumberOfPatients) {
+            int query;
+            query = in.nextInt();
+            if (query == 1) {
+                NumberOfAdmittedPatients += addHealthCare();
+            }else if (query == 2) {
+                camp.removeAdmitted();
+            }else if (query == 3) {
+                institutes.removeInstitutes();
+            }else if (query == 4) {
+                camp.stillInCamp();
+            }else if (query == 5) {
+                institutes.admittingCurrently();
+            }else if (query == 6) {
+                String name = in.next();
+                institutes.displayInstitute(name);
+            }else if (query == 7) {
+                int ID = in.nextInt();
+                camp.displayPatient(ID);
+            }else if (query == 8) {
+                camp.displayAllPatient();
+            }else if (query == 9) {
+                String name = in.next();
+                institutes.displayPatientOfInstitutes(name);
+            }
+        }
+
+    }
+
+    public static int addHealthCare() {
+
+        String name = in.next();
+
+        System.out.print("Temperature Criteria - ");
+        int maxTemperature = in.nextInt();
+
+        System.out.print("Oxygen Levels - ");
+        int minOxygen = in.nextInt();
+
+        System.out.print("Number of Available beds - ");
+        int numberOfBeds = in.nextInt();
+
+        institutes.addInstitute(new Institute(name, maxTemperature, minOxygen, numberOfBeds));
+        Institute institute = institutes.getLast();
+
+        institute.display();
+
+        ArrayList<Patient> canAdd = camp.firstCriteria(minOxygen);
+
+        if (numberOfBeds < canAdd.size()) {
+            canAdd = camp.secondCriteria(canAdd, maxTemperature, numberOfBeds);
+        }
+
+        for (Patient patient : canAdd) {
+            System.out.print("Recovery days for admitted patients ID " + patient.getID() + " - ");
+            int recoveryTime = in.nextInt();
+            patient.setRecoveryTime(recoveryTime);
+            patient.setAdmitted(true);
+        }
+        institute.addPatients(canAdd);
+        return canAdd.size();
+    }
+
+}
